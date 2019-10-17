a30d8bc877510a4b6a28127600edf0b4152cf5c8
mjmazur <mmazur5@uwo.ca>
2019-10-16 20:09:18 -0400

    Update

--- TrailLoss.py
+++ TrailLoss.py
@@ -4,12 +4,14 @@ from matplotlib import pyplot as plt
 
 hsteps = 12
 vsteps = 8
+asteps = 10
 
 # v = np.linspace(1,72,vsteps+1)
 # h = np.linspace(70,130,hsteps+1)
 
 v = [1,5,11.2,20,30,40,50,60,72]
 h = [70,75,80,85,90,95,100,105,110,115,120,125,130]
+a = [1,5,10,15,20,30,40,50,60,70,90]
 
 framerate = 32 # frame rate in fps
 pangsize = 103 # detector pixel size in arcsecs
@@ -22,11 +24,17 @@ for i in range(hsteps+1):
 
 traillength = np.zeros((vsteps+1,hsteps+1)) # rows = height list, columns = speed list
 
+angle = 90
+
 for i in range(vsteps+1):
 	for j in range(hsteps+1):
-		traillength[i,j] = (v[i]*1000/psize[j] / framerate)
+		traillength[i,j] = (v[i]*1000/psize[j] / framerate)*np.sin((2*np.pi/360)*angle)
 
-print(np.shape(traillength))
+trailat100km = np.zeros((vsteps+1,asteps+1))
+
+for i in range(vsteps+1):
+	for j in range(asteps+1):
+		trailat100km[i,j] = (v[i]*1000/(math.tan((2*np.pi/360)*pangsize/(2*3600))*100*2*1000) / framerate)*np.sin((2*np.pi/360)*a[j])
 
 # Above calculates for a meteor that is orthogonal to the look direction. A real meteor will
 # typically not move orthogonal to the camera look direction. To deal with this, we need to
@@ -34,13 +42,10 @@ print(np.shape(traillength))
 
 angrate = np.zeros((vsteps+1,hsteps+1))
 
-entryangle = 90
-
 for i in range(vsteps+1):
 	for j in range(hsteps+1):
-		angrate[i,j] = math.atan((v[i]/2)/h[j])*2*np.sin((2*np.pi/360)*entryangle) # angular rate of motion of trail in degrees / second
+		angrate[i,j] = math.atan((v[i]/2)/h[j])*2*np.sin((2*np.pi/360)*angle) # angular rate of motion of trail in degrees / second
 
-print(angrate)
 
 # length = np.sin((2*np.pi/360)*entryangle)
 
@@ -79,5 +84,25 @@ plt.legend()
 plt.tight_layout()
 
 plt.savefig('TrailLengths.png', dpi=300)
+
+plt.show()
+
+plt.figure(figsize=(8,4))
+
+for i in range(asteps+1):
+	plt.plot(v,trailat100km[:,i])
+	plt.text(72.5,max(trailat100km[:,i]-0.5),str(int(a[i])) + ' deg', size=8)
+
+#plt.hlines(motionlimit,0,72, linestyle='--')
+plt.plot((11.2,11.2),(0,max(trailat100km[:,asteps])), linestyle=':', color='black', label='11.2 km/s')
+plt.plot((0,72),(motionlimit,motionlimit), linestyle='--', color='black', label=str(motionlimit) + ' Pixel Threshold')
+
+plt.xlabel('Speed (km/s)', size=15)
+plt.ylabel('Trail Length (pixels/frame)', size=15)
+plt.xlim(0,72)
+plt.ylim(0,max(trailat100km[:,asteps]))
+
+plt.legend()
+plt.tight_layout()
 
 plt.show()
