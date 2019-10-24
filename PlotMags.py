a30142580444eb60d5eea33aecbc76c8a032f8ad
mjmazur <mmazur5@uwo.ca>
2019-10-24 16:17:01 -0400

    Update PlotMags.py

--- PlotMags.py
+++ PlotMags.py
@@ -62,11 +62,11 @@ BmR = df.BMAG - df.RMAG
 BmG = df.BMAG - df.GMAG
 GmR = df.GMAG - df.RMAG
 
-imag = np.array([10.173, 11.649, 11.154, 11.819])
-pmag = np.array([11.721, 13.270, 13.231, 14.281])
+imag = np.array([10.173, 11.649, 11.154, 11.819, 10.779, 11.02, 9.882, 10.161])
+pmag = np.array([11.721, 13.270, 13.231, 14.281, 13.833, 13.347, 12.362, 12.750])
 # imag = 10.173
 # pmag = 11.721
-ci = np.array([0.458, 0.481, 0.804, 1.096])
+ci = np.array([0.458, 0.481, 0.804, 1.096, 1.484, 0.932, 1.187, 1.208])
 
 def magcalc(params):
     cmag = imag + params[0]*ci + params[1] # cmag = calculated mag, ci = colour index
