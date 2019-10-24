2461e43fcd89f6cfef7495e3b28c87aae23f46c0
mjmazur <mmazur5@uwo.ca>
2019-10-23 21:56:01 -0400

    Update PlotMags.py

--- PlotMags.py
+++ PlotMags.py
@@ -70,7 +70,7 @@ def magcalc(params):
     cmag = imag + params[0]*ci + params[1] # cmag = calculated mag, ci = colour index
     return cmag
 
-def res(params, args):
+def res(params, imag, ci, pmag):
     # cmag = magcalc(params)
     cmag = imag + params[0]*ci + params[1]
     return pmag - cmag # pmag = photometric mag
